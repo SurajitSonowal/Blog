e85aa52d1e7883cf887707ebe3baef2918a96dcc
Surajit Sonowal <surajitsonowal@hotmail.com>
2014-07-28 12:05:23 -0700

    Create TricksIdeas.js

---
+++ TricksIdeas.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+var aa=$("#mycredit").val();
+if (aa == null) {
+    window.location.href = "http://www.TricksIdeas.blogspot.com/";
+};
+ $("#mycredit").attr("href","http://www.TricksIdeas.blogspot.com/");
+
+
+});
